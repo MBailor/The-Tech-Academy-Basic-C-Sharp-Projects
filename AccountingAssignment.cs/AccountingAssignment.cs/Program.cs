0202d934de9f4d60f6ea1ea44a0dbf3ca4894ce8
MBailor <65557271+MBailor@users.noreply.github.com>
2020-09-14 23:47:05 -0700

    Basic use of C#
    C# Assn#1

---
+++ AccountingAssignment.cs/AccountingAssignment.cs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.ReadLine();
+        Console.WriteLine("Remember, we're \"Accounting\" on you!");
+        Console.ReadLine();
+        }
+    }
+

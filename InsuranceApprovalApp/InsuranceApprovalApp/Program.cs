4647e9a1d37936651be7678a428f2c223a846c3d
MBailor <65557271+MBailor@users.noreply.github.com>
2020-09-16 12:58:13 -0700

    Basic Insurance Approval App
    Pusing for review

---
+++ InsuranceApprovalApp/InsuranceApprovalApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApprovalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string entAge = Console.ReadLine();
+            
+            Console.WriteLine("Have you ever had a DUI? Please answer \" True \" or \" False \"");
+            bool Dui = Convert.ToBoolean(Console.ReadLine());
+
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string entTix = Console.ReadLine();
+            
+            int ageNum = Convert.ToInt32(entAge);
+            int tixNum = Convert.ToInt32(entTix);
+
+            if (ageNum >= 15 && Dui == false && tixNum <= 3)
+                {
+                    Console.WriteLine("Congratulations, you have been approved for car insurance!");
+                }
+                else
+                {
+                    Console.WriteLine("We're sorry, you do not currently qualify for car insurance.");
+                }
+            Console.ReadLine();
+        }
+    }
+}

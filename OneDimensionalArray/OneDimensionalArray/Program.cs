5413f9f87dbf8f3bed314d1fd6b1547538741bc2
MBailor <65557271+MBailor@users.noreply.github.com>
2020-09-18 14:23:44 -0700

    Basic usage of one dimensional aray, and lists. as well as try and catch
    Pusing for review

--- OneDimensionalArray/OneDimensionalArray/Program.cs
+++ OneDimensionalArray/OneDimensionalArray/Program.cs
@@ -1,27 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 
-    class Program
+class Program
+{
+    static void Main()
     {
-        static void Main()
+
+        int[] numArray = new int[6];
+        numArray[0] = 1;
+        numArray[1] = 2;
+        numArray[2] = 3;
+        numArray[3] = 4;
+        numArray[4] = 5;
+        numArray[5] = 6;
+
+
+        try
         {
-        
-        int[] numArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        Console.WriteLine("Please select and index of the number array(0-9)");
-        int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please select an index of the number array(0-5)");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool number1 = Convert.ToBoolean(numArray[number]);
 
+            if (number1)
+            {
+                Console.WriteLine(numArray[number]);
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("You have selected an index that doesn't exist.");
+        }
 
-        Console.WriteLine(numArray[number]);
         Console.ReadLine();
 
-        string[] strArray = new string[] { "H", "E", "L", "L", "O", "N", "E", "W", "M", "A", "N" };
-        Console.WriteLine("Please select and index of the string array(0-10)");
-        int word = Convert.ToInt32(Console.ReadLine());
+        try
+        {
+            string[] strArray = new string[] { "Happy", "Elephant", "Linguini", "Levitate", "Open", "Nacho", "Earwig", "Wall", "Map", "Alpine", "N" };
+            Console.WriteLine("Please select and index of the string array(0-10)");
+            int word = Convert.ToInt32(Console.ReadLine());
+            bool entWord = Convert.ToBoolean(word);
 
-
-        Console.WriteLine(strArray[word]);
+            if (entWord)
+            {
+                Console.WriteLine(strArray[word]);
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("You have selected an index that doesn't exist.");
+        }
         Console.ReadLine();
 
-    } 
-    }
+        try
+        {
+            List<string> strList = new List<string>();
+            strList.Add("Snare Drum");
+            strList.Add("Baseball");
+            strList.Add("Bong");
+            strList.Add("Fish");
+            strList.Add("Pancake");
+            strList.Add("Kitten");
+            strList.Add("Slide");
+            strList.Add("Triangle");
+            strList.Add("Eggplant");
+
+            Console.WriteLine("Chose and item from the list by it's index number(0-8)");
+            int choseItem = Convert.ToInt32(Console.ReadLine());
+            bool itemChose = Convert.ToBoolean(choseItem);
+
+            if (itemChose)
+            {
+                Console.WriteLine(strList[choseItem]);
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("You have selected an index that doesn't exist.");
+        }
+        Console.ReadLine();
+            
+        }
+} 
 
